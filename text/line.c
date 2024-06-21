80983d045ec0f21514ef76fd766bc46c93319384
aquryle <aquryle@gmail.com>
2024-06-21 22:26:24 +0900

    add line.c

---
+++ text/line.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define ASCII_LF	(0x0Au)
+#define ASCII_CR	(0x0Du)
+
+
+/**
+ * @brief
+ *
+ * @param buf
+ * @return uint16_t
+ */
+uint16_t seek_crlf(uint8_t *const buf)
+{
+	uint16_t i;
+
+
+
+	return i;
+}
+
+
+
